95fa925488b9119640e2472e90117085bc876de3
Damilola Okunlola <okunlolatawakalitu61@gmail.com>
2024-02-27 00:50:38 +0100

    add function renderListItems() to display the items after form submission

---
+++ index.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('form')
+let inputField = document.getElementById('input-field')
+const clearListBtn = document.getElementById('clear')
+const item = document.querySelector('.item')
+
+
+let renderListItems = e => {
+    e.preventDefault()
+    document.getElementById('list').innerHTML +=  `
+
+        <div class="item">
+            <p>${inputField.value}</p>
+            <span>
+                <i class="fa-solid fa-pen-to-square"></i>
+                <i class="fa-solid fa-trash"></i>
+            </span>
+        </div>
+     
+    `
+}
+
+form.addEventListener('submit', renderListItems)
+
