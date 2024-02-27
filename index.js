c6892770e56b543271662df2cdc0e905ce64098e
Damilola Okunlola <okunlolatawakalitu61@gmail.com>
2024-02-27 01:22:27 +0100

    add function edit to the edit icon

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ let Delete = e => {
     e.parentElement.parentElement.remove()
 }
 
+let edit = e => {
+    inputField.value = e.parentElement.previousElementSibling.textContent
+
+    e.parentElement.parentElement.remove()
+}
+
 let renderListItems = e => {
     e.preventDefault()
     document.getElementById('list').innerHTML +=  `
@@ -14,7 +20,7 @@ let renderListItems = e => {
         <div class="item">
             <p>${inputField.value}</p>
             <span>
-                <i class="fa-solid fa-pen-to-square"></i>
+                <i class="fa-solid fa-pen-to-square" onclick="edit(this)"></i>
                 <i class="fa-solid fa-trash" onclick="Delete(this)"></i>
             </span>
         </div>
