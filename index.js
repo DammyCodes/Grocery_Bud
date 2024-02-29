ea9cb7d936571dc3385dc766a2d2b9901f0dcefc
Damilola Okunlola <okunlolatawakalitu61@gmail.com>
2024-02-29 21:29:41 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const form = document.getElementById('form')
 let inputField = document.getElementById('input-field')
 const clearItemsBtn = document.getElementById('clear')
-const item = document.querySelector('.item')
 
 let Delete = e => {
     e.parentElement.parentElement.remove()
@@ -15,18 +14,22 @@ let edit = e => {
 
 let renderListItems = e => {
     e.preventDefault()
-    document.getElementById('list').innerHTML +=  `
 
-        <div class="item">
-            <p>${inputField.value}</p>
-            <span>
-                <i class="fa-solid fa-pen-to-square" onclick="edit(this)"></i>
-                <i class="fa-solid fa-trash" onclick="Delete(this)"></i>
-            </span>
-        </div>
-     
-    `
+    if (inputField.value) {
+        document.getElementById('list').innerHTML +=  `
+    
+            <div class="item">
+                <p>${inputField.value}</p>
+                <span>
+                    <i class="fa-solid fa-pen-to-square" onclick="edit(this)"></i>
+                    <i class="fa-solid fa-trash" onclick="Delete(this)"></i>
+                </span>
+            </div>
+         
+        `
+    }
     inputField.value = ''
+   
 }
 
 form.addEventListener('submit', renderListItems)
